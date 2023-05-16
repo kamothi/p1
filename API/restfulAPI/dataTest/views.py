095f5d9d48b90917a33f6df0d48763e34141ac7b
Hyundol <58063456+Hyundol@users.noreply.github.com>
2023-05-16 11:06:14 +0900

    Update views.py

--- API/restfulAPI/dataTest/views.py
+++ API/restfulAPI/dataTest/views.py
@@ -9,6 +9,14 @@ from rest_framework.response import Response
 from .createjwt import generate_jwt_token
 import jwt
 from django.conf import settings
+from .models import challenge
+from .serializer import userSerializer
+from .serializer import challengeSerializer
+from .serializer import mainchallengeSerializer
+from .serializer import smallchallengeSerializer
+from .serializer import contentchallengeSerializer
+from .serializer import updatechallengeSerializer
+from .serializer import rankchallengeSerializer
 
 @csrf_exempt
 def user_list(request):
@@ -98,3 +106,50 @@ def board_list(request):
         # Board.save()
         return JsonResponse(data, status=201) # 정상 응답 201
         # return JsonResponse(serializer.errors, status=400) # 모델에 일치하지 않는 데이터일 경우
+        
+
+def challenge_list(request):
+    if request.method == 'GET':  # GET 방식일 때
+        query_set = challenge.objects.all()  # ORM으로 Users의 모든 객체 받아옴
+        serializer = challengeSerializer(query_set, many=True)  # JSON으로 변환
+        return JsonResponse(serializer.data, safe=False)  # JSON타입의 데이터로 응답
+
+    elif request.method == 'POST': # POST방식일 때
+        data = JSONParser().parse(request) # 요청들어온 데이터를 JSON 타입으로 파싱
+        serializer = challengeSerializer(data=data) # Serializer를 사용해 전송받은 데이터를 변환하기 위함
+        if serializer.is_valid(): # 생성한 모델과 일치하면
+            serializer.save() # 데이터 저장
+            return JsonResponse(serializer.data, status=201) # 정상 응답 201
+        return JsonResponse(serializer.errors, status=400) # 모델에 일치하지 않는 데이터일 경우
+
+def challenge_home(request):
+    if request.method == 'GET':  # GET 방식일 때
+        query_set = challenge.objects.all()  # ORM으로 Users의 모든 객체 받아옴
+        serializer = mainchallengeSerializer(query_set, many=True)  # JSON으로 변환
+        return JsonResponse(serializer.data, safe=False)  # JSON타입의 데이터로 응답
+
+def challenge_smallcategory(request):
+    if request.method == 'GET':  # GET 방식일 때
+        query_set = challenge.objects.all()  # ORM으로 Users의 모든 객체 받아옴
+        serializer = smallchallengeSerializer(query_set, many=True)  # JSON으로 변환
+        return JsonResponse(serializer.data, safe=False)  # JSON타입의 데이터로 응답
+
+def challenge_content(request,pk):
+    object = challenge.objects.get(pk=pk)
+    if request.method == "GET":
+        serializer = contentchallengeSerializer(object)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'PUT': # POST방식일 때
+        update_data = JSONParser().parse(request) # 요청들어온 데이터를 JSON 타입으로 파싱
+        serializer = updatechallengeSerializer(data=update_data) # Serializer를 사용해 전송받은 데이터를 변환하기 위함
+        if serializer.is_valid(): # 생성한 모델과 일치하면
+            serializer.save() # 데이터 저장
+            return JsonResponse(serializer.data, status=201) # 정상 응답 201
+        return JsonResponse(serializer.errors, status=400) # 모델에 일치하지 않는 데이터일 경우
+
+def challenge_rank(request):
+    if request.method == 'GET':  # GET 방식일 때
+        query_set = challenge.objects.all()  # ORM으로 Users의 모든 객체 받아옴
+        serializer = rankchallengeSerializer(query_set, many=True)  # JSON으로 변환
+        return JsonResponse(serializer.data, safe=False)  # JSON타입의 데이터로 응답
