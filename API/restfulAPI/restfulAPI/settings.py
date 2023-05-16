b3436f732a7d9166b8c89f95a26b2e3c1643a4b3
Hyundol <58063456+Hyundol@users.noreply.github.com>
2023-05-16 11:03:24 +0900

    Update settings.py

--- API/restfulAPI/restfulAPI/settings.py
+++ API/restfulAPI/restfulAPI/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dataTest',
+    'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_jwt',
 ]
