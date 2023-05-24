0fa6c79cc097754449bf0e460ee215d44980de8b
Hyundol <jackrkd022244@gmail.com>
2023-05-24 15:56:45 +0900

    최종

---
+++ restfulAPI/dataTest/createjwt.py
@@ -0,0 +1,11 @@
+import jwt
+from datetime import datetime, timedelta
+from django.conf import settings
+
+def generate_jwt_token(user_id):
+    payload = {
+        'user_id': user_id,
+        'exp': datetime.utcnow() + timedelta(days=1),
+        'iat': datetime.utcnow()
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
---
+++ restfulAPI/dataTest/serializer.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from .models import Users,Customer
+from .models import board
+from .models import challenge
+
+class userSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ["name","phone_number","job_position","age"]
+
+class signSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class boardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = board
+        fields = ["title", "content", "comment", "update_date"]
+# 제목/ 게시글 내용/ 댓글 수/ 좋아요 수/ 게시글 올린 시간  // 익명은 프론트에서
+
+class writeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = board
+        fields = ["title", "content", "views","comment","userId"]
+ 
+class challengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = challenge
+        fields = "__all__"
+
+class mainchallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = challenge
+        fields = ["subject", "Bigcategory", "smallcategory", "challenge_id"]
+
+class smallchallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = challenge
+        fields = ["subject", "rate", "views"]
+
+class contentchallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = challenge
+        fields = ["subject", "content", "address", "challenge_id", "views"]
+
+class updatechallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = challenge
+        fields = ["rate", "views"]
+
+class rankchallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = challenge
+        fields = ["subject", "like"]
